3d5e1319f736d9dade5f10d3752b24c7f33b17a6
Ezra Mabatu <ezramabatu@gmail.com>
2025-06-23 22:17:10 +0300

    Create vec2.h

---
+++ include/vec2.h
@@ -0,0 +1,18 @@
+#pragma once
+struct vec2 {
+    float x, y;
+
+    vec2(float x = 0.0f, float y = 0.0f) : x(x), y(y) {}
+
+    vec2 operator+(const vec2& other) const {
+        return vec2(x + other.x, y + other.y);
+    }
+
+    vec2 operator*(float scalar) const {
+        return vec2(x * scalar, y * scalar);
+    }
+
+    vec2 operator/(float scalar) const {
+        return vec2(x / scalar, y / scalar);
+    }
+};
