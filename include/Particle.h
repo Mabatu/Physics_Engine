41c48d9df7c8514a49d0fa643c173c8c4adbf136
Ezra Mabatu <ezramabatu@gmail.com>
2025-06-24 20:12:20 +0300

    Update Particle.h

--- include/Particle.h
+++ include/Particle.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "vec2.h"
+#include "globals.h"
 
 class Particle
 {
@@ -7,8 +8,10 @@ public:
 	vec2 Position;
 	vec2 Velocity;
 	vec2 Force;
+	vec2 Acceleration;
 	float Mass;
 
 	void Update(float dt);
 	void ApplyForce(vec2 ExternalForce, float dt);
+	void ApplyImpulse(vec2 ImpulsiveForce, float dt);
 };
