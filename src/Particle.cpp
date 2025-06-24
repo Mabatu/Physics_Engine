de11d2706e25775bd04f947388ac3d9e6cbf0975
Ezra Mabatu <ezramabatu@gmail.com>
2025-06-24 23:14:05 +0300

    Update Particle.cpp

--- src/Particle.cpp
+++ src/Particle.cpp
@@ -5,7 +5,7 @@
 
 void Particle::Update(float dt) {
 	Force = earth_gravity * Mass;
-	if (Force.y <= 0.0f) {
+	if (Position.y <= 0.0f) {
 		Force.y = 0.0f;
 	}
 	else {
@@ -17,18 +17,25 @@ void Particle::Update(float dt) {
 	
 }
 void Particle::ApplyForce(vec2 ExternalForce, float dt) {
-	Force = (earth_gravity * Mass) + ExternalForce;
-/*  
-   if (Force.y <= 0.0f) {
-		Force.y = 0.0f;
+	if (ExternalForce.x == 0.0 && ExternalForce.y == 0.0) {
+		Acceleration = earth_gravity;
+		Position = Position + Velocity * dt + Acceleration * (dt * dt) / 2;
+		Velocity = Velocity + Acceleration * dt;
 	}
+	
+	/*if (Position.y <= 0.0f) {
+		 Force.y = 0.0f;
+	 }
+	 else {
+		 Force.y = Force.y;
+	 }*/
 	else {
-		Force.y = Force.y;
+		Force = (earth_gravity * Mass) + ExternalForce;
+
+		Acceleration = Force / Mass;
+		Position = Position + Velocity * dt + Acceleration * (dt * dt) / 2;
+		Velocity = Velocity + Acceleration * dt;
 	}
-*/
-	Acceleration = Force / Mass;
-	Position = Position + Velocity * dt + Acceleration * (dt * dt) / 2;
-	Velocity = Velocity + Acceleration * dt;	
 }
 void Particle::ApplyImpulse(vec2 ImpulsiveForce, float dt) {
 	//Impulse can be defined as change in momentum: F * dt = mv - mu 
