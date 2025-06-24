6422121b973b78f49e6e1f502718bfff6da8de5e
Ezra Mabatu <ezramabatu@gmail.com>
2025-06-24 12:20:14 +0300

    Rename RigidBody.cpp to Particle.cpp Renaming classes

--- src/Particle.cpp
+++ src/Particle.cpp
@@ -1,11 +1,11 @@
-#include "RigidBody.h"
+#include "Particle.h"
 
-void RigidBody::Update(float dt) {
+void Particle::Update(float dt) {
 	Force = earth_gravity * Mass;
 	Velocity = Velocity + Force / Mass * dt;
 	Position = Position + Velocity * dt;
 }
-void RigidBody::ApplyForce(vec2 ExternalForce, float dt) {
+void Particle::ApplyForce(vec2 ExternalForce, float dt) {
 	Force = (earth_gravity * Mass) + ExternalForce;
 	Velocity = Velocity + Force / Mass * dt;
 	Position = Position + Velocity * dt;
