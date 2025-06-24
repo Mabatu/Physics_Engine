a181b69342d2cc13d5a96862dabeca39fcabaf4d
Ezra Mabatu <ezramabatu@gmail.com>
2025-06-24 12:27:38 +0300

    Update Particle.cpp

--- src/Particle.cpp
+++ src/Particle.cpp
@@ -1,12 +1,29 @@
+#include <iostream>
+#include <cmath>
 #include "Particle.h"
+#include "globals.h"
 
 void Particle::Update(float dt) {
 	Force = earth_gravity * Mass;
+	if (Force.y <= 0.0f) {
+		Force.y = 0.0f;
+	}
+	else {
+		Force.y = Force.y;
+	}	
 	Velocity = Velocity + Force / Mass * dt;
 	Position = Position + Velocity * dt;
+	
 }
 void Particle::ApplyForce(vec2 ExternalForce, float dt) {
 	Force = (earth_gravity * Mass) + ExternalForce;
+	if (Force.y <= 0.0f) {
+		Force.y = 0.0f;
+	}
+	else {
+		Force.y = Force.y;
+	}
 	Velocity = Velocity + Force / Mass * dt;
 	Position = Position + Velocity * dt;
 }
+
